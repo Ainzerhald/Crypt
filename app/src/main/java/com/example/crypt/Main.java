b6d47f88d801e4e5703e58569fc9e8d909f908f8
Ainzerhald <89225940713@mail.ru>
2021-03-01 23:23:54 +0300

    Шифровка файла любого размера

--- app/src/main/java/com/example/crypt/Main.java
+++ app/src/main/java/com/example/crypt/Main.java
@@ -1,6 +1,7 @@
 package com.example.crypt;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -14,11 +15,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Toast;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -40,25 +39,7 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ProgressBar bar = findViewById(R.id.progressBar);
-        new AsyncTask<Void, Void, Void>(){
 
-            @Override
-            protected Void doInBackground(Void... voids) {
-                new Authorization().Authorization("", "", 0, "");
-                return null;
-            }
-
-            protected void onPreExecute() {
-                super.onPreExecute();
-                bar.setVisibility(View.VISIBLE);
-            }
-
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                bar.setVisibility(View.INVISIBLE);
-            }
-        }.execute();
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);//Запрос на доступ к файлам
         if(!Files.exists(Paths.get(dir))){//Создание папки CRYPT в случае его отсутствия
             try {
@@ -75,59 +56,37 @@ public class Main extends AppCompatActivity {
         startActivityForResult(file, 1);
     }
 
+    FileDescriptor fd = null;
+
     public void onActivityResult (int requestCode, int resultCode, Intent data ) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
             ParcelFileDescriptor uri = getContentResolver().openFileDescriptor(data.getData(), "r");
-            FileDescriptor fd = uri.getFileDescriptor();//Получение дескриптора файла
+            fd = uri.getFileDescriptor();//Получение дескриптора файла
             ContentResolver cR = getContentResolver();
             MimeTypeMap mime = MimeTypeMap.getSingleton();
             type = mime.getExtensionFromMimeType(cR.getType(data.getData()));//Получение расширение файла
-            FileInputStream input = new FileInputStream(fd);
-            buffer = null;
-            buffer = new byte[input.available()];
-            try {
-                input.read(buffer, 0, buffer.length);//Получение массива байт кода файла
-                input.close();
-                file = 1;
+            file = 1;
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    byte[] symbol;
+    protected byte[] symbol;
 
-    public void execute(View view) throws IOException {
+    public void execute(View view) {
         EditText key = findViewById(R.id.key);
         symbol = key.getText().toString().getBytes();
         if(!key.getText().toString().equals("")){
             if(key.length() > 5){
                 if(file != 0){
-                    String file_name = "";
-                    Date date = new Date();
-                    file_name += String.valueOf(date.getTime());
                     RadioButton check = findViewById(R.id.crypt);
                     if(check.isChecked()){
-                        buffer = crypt(buffer, key.getText().toString().toUpperCase());
-                        file_name += "(crypt).";
+                        crypt(key.getText().toString());
                     }
                     else{
-                        buffer = decrypt(buffer, key.getText().toString().toUpperCase());
-                        file_name += "(decrypt).";
+                        decrypt(key.getText().toString());
                     }
-                    FileOutputStream fout = new FileOutputStream(dir + file_name + type);
-                    fout.write(buffer, 0, buffer.length);
-                    fout.close();
-                    buffer = null;
-                    symbol = null;
-                    key.setText(null);
-                    file = 0;
-                    Toast.makeText(this, "Операция выполнена", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Toast.makeText(this, "Выберите файл", Toast.LENGTH_SHORT).show();
@@ -142,28 +101,92 @@ public class Main extends AppCompatActivity {
         }
     }
 
-    protected byte[] crypt(byte[] buf, String key){
-        int control = 0;
-        for(int i = 0; i < buffer.length; i++) {
-            if(control == key.length()) {
-                control = 0;
-            }
-            buffer[i] = (byte) (buffer[i] + symbol[control]);
-            control++;
-        }
-        return buf;
+    protected void crypt(String key){
+        process(key, true);
     }
 
-    protected byte[] decrypt(byte[] buf, String key){
-        int control = 0;
-        for(int i = 0; i < buffer.length; i++) {
-            if(control == key.length()) {
-                control = 0;
+    protected void decrypt(String key){
+        process(key, false);
+    }
+
+    public void process(String key, boolean crypt) {
+        ProgressBar bar = findViewById(R.id.progressBar);
+        new AsyncTask<Void, Void, Void>(){
+
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected Void doInBackground(Void... voids) {
+                String file_name = "";
+                Date date = new Date();
+                file_name += date.getTime();
+                FileInputStream input = new FileInputStream(fd);
+                FileOutputStream out = null;
+                try {
+                    out = new FileOutputStream(dir + file_name + "." + type);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                int t = -1;
+                if(crypt){
+                    t = 1;
+                }
+                int bit = 1024;
+                int key_len = 0;
+                symbol = key.getBytes();
+                try {
+                    int size = input.available();
+                    int check = 0;
+                    while(size > bit) {
+                        size -= bit;
+                        check++;
+                    }
+                    boolean control = false;
+                    int dop = 0;
+                    if(size != 0) {
+                        control = true;
+                        dop = size;
+                    }
+                    buffer = new byte[bit];
+                    for(int i = 0; i < check; i++) {
+                        input.read(buffer, 0, buffer.length);
+                        for(int g = 0; g < buffer.length; g++) {
+                            if(key_len == key.length()){
+                                key_len = 0;
+                            }
+                            buffer[g] = (byte) (buffer[g] + t * symbol[key_len]);
+                            key_len++;
+                        }
+                        out.write(buffer, 0, buffer.length);
+                    }
+                    if(control) {
+                        buffer = new byte[dop];
+                        input.read(buffer, 0, buffer.length);
+                        for(int i = 0; i < buffer.length; i++){
+                            if(key_len == key.length()){
+                                key_len = 0;
+                            }
+                            buffer[i] = (byte) (buffer[i] + t * symbol[key_len]);
+                        }
+                        out.write(buffer, 0, buffer.length);
+                        input.close();
+                        out.close();
+                    }
+                } catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+                return null;
             }
-            buffer[i] = (byte) (buffer[i] - symbol[control]);
-            control++;
-        }
-        return buf;
+
+            protected void onPreExecute() {
+                super.onPreExecute();
+                bar.setVisibility(View.VISIBLE);
+            }
+
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                bar.setVisibility(View.INVISIBLE);
+            }
+        }.execute();
     }
 
 }
